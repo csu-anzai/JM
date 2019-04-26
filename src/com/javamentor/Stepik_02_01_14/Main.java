a34febb5446d003dabd87381ee52dea173c68dae
khilkevich igor <khilkevichigor@gmail.com>
2019-04-27 00:49:48 +0300

    битовые операции

--- src/com/javamentor/Stepik_02_01_14/Main.java
+++ src/com/javamentor/Stepik_02_01_14/Main.java
@@ -7,13 +7,39 @@
  */
 package com.javamentor.Stepik_02_01_14;
 
-public class Main {
-    public static void main (String[] args) {
+import java.util.ArrayList;
+import java.util.Arrays;
 
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(flipBit(200, 3));
     }
 
     public static int flipBit(int value, int bitIndex) {
-        value = ~bitIndex;
-        return value;
+        String s = Integer.toBinaryString(value);   //получили строку (1 и 0) из int
+        System.out.println("1) " + s);
+        char[] arr = s.toCharArray();
+        System.out.println("2) " + arr);
+        char g = arr[bitIndex];   //в символ кладем символ под индексом bitIndex
+        System.out.println("3) " + g);
+        int m = Character.getNumericValue(g);    //переводим этот символ в int и инвертируем
+        System.out.println("4) " + m);
+
+        if (m == 0) {
+            m = 1;
+        } else {
+            m = 0;
+        }
+        System.out.println("5) " + m);
+        char i = (char)('0' + m);   //переводим int в символ
+        System.out.println("6) " + i);
+        arr[bitIndex] = i;  //этот символ кладем в массив символов под индексом bitIndex
+        System.out.println("7) " + arr[bitIndex]);
+        String tt = new String(arr);    //переводим в строку
+        System.out.println("8) " + tt);
+        int t = Integer.parseInt(tt);   //переводим в Int
+        System.out.println("9) " + t);
+        return t;   //возвращием Int
     }
 }
+
---
+++ src/com/javamentor/Stepik_02_01_14/Test.java
@@ -0,0 +1,4 @@
+package com.javamentor.Stepik_02_01_14;
+
+public class Test {
+}
