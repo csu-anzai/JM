e2de4428fdca9c7f587cad7ac93f2bf81f32b167
khilkevich igor <khilkevichigor@gmail.com>
2019-04-28 01:15:38 +0300

    XOR, битовые маски

--- src/com/javamentor/Stepik_02_01_14/Main.java
+++ src/com/javamentor/Stepik_02_01_14/Main.java
@@ -12,35 +12,50 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(flipBit(1,0));
+        System.out.println(flipBit(22,10));
     }
 
     public static int flipBit(int value, int bitIndex) {
 
-        String s = String.format("%32s", Integer.toBinaryString(value)).replace(' ', '0'); //чтобы не отбрасывать нули
-        System.out.println(s);
+        System.out.println(1<<5);
 
-        char[] arr = s.toCharArray();   //создаем массив и инициализируем символами
-
-        int m = Character.getNumericValue(arr[32-bitIndex]);    //переводим этот символ в int и инвертируем
-        System.out.println(m);
-
-        if (m == 0) {
-            m = 1;
-        } else {
-            m = 0;
-        }
-
-        char i = (char)('0' + m);   //КОСТЫЛЬ. переводим int в char
-        System.out.println(i);
-
-        arr[32-bitIndex] = i;  //этот символ кладем в массив символов под индексом bitIndex
-
-        String tt = new String(arr);    //переводим массив char в строку
-        System.out.println(tt);
-
-        int t = Integer.parseInt(tt, 2);   //переводим из двоичного в десятичное
-
-        return t;   //возвращием Int
+//        String s = String.format("%32s", Integer.toBinaryString(value)).replace(' ', '0'); //чтобы не отбрасывать нули
+//        System.out.println(s);
+//
+//        char[] arr = s.toCharArray();   //создаем массив и инициализируем символами
+//
+//        int m = Character.getNumericValue(arr[32-bitIndex]);    //переводим этот символ в int и инвертируем
+//        System.out.println(m);
+//
+//        if (m == 0) {
+//            m = 1;
+//        } else {
+//            m = 0;
+//        }
+//
+//        char i = (char)('0' + m);   //КОСТЫЛЬ. переводим int в char
+//        System.out.println(i);
+//
+//        arr[32-bitIndex] = i;  //этот символ кладем в массив символов под индексом bitIndex
+//
+//        String tt = new String(arr);    //переводим массив char в строку
+//        System.out.println(tt);
+//
+//        int t = Integer.parseInt(tt, 2);   //переводим из двоичного в десятичное
+//
+//        return t;   //возвращием Int
+/**
+ * Логика:
+ * 1) Битовая маска. (bitIndex-1) - получаем правильный индекс бита, в данном случае -1 т.к. начинаем по условию
+ * с 1 а не с 0
+ * 2) Битовая маска. (1<<) - первый бит десятичного числа 1 (00000000 00000000 00000000 00000001) двигаем влево
+ * на правильный индекс
+ * 3) выполняем XOR (логическое НЕ-ИЛИ) между value и битовой маской
+ *
+ * Вопрос1: почему именно 1<<. Потому что так можно получить одну 1 в битовой маске для изменения value в этом бите.
+ * Вопрос2: почему именно XOR. Потому что XOR меняет бит в value именно в bitIndex на противоположный.
+ */
+        return value^(1<<(bitIndex-1)); //списал, stackoverflow
     }
 }
+
