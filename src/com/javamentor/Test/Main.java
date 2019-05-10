b3a5ddf40aad51c9c5b20eecae1a0d60674d90f5
khilkevich igor <khilkevichigor@gmail.com>
2019-05-11 00:30:49 +0300

    my tests

---
+++ src/com/javamentor/Test/Main.java
@@ -0,0 +1,13 @@
+package com.javamentor.Test;
+
+public class Main {
+    public static void main(String[] args) {
+        int j = 35;
+        System.out.println(j >> 2);
+        System.out.println(j);
+
+        int i = 35;
+        System.out.println(i >>= 2);
+        System.out.println(i);
+    }
+}
