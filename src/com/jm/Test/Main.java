f39cb705a726058ec15b307eb600ae0e016db98c
khilkevich igor <khilkevichigor@gmail.com>
2019-06-01 00:04:42 +0300

    test

--- src/com/jm/Test/Main.java
+++ src/com/jm/Test/Main.java
@@ -1,134 +1,21 @@
 package com.jm.Test;
 
-
-import com.jm.Stepik_03_05_03.*;
+import java.util.Arrays;
 
 public class Main {
+    public static void main(String[] args) {
+        Main m = new Main();
+        Boolean[] booleans = new Boolean[3];
 
-    interface TextAnalyzer {
+        System.out.println(Arrays.toString(booleans));
 
-        Label processText(String text);
+        System.out.println(Arrays.toString(m.boolIn(booleans)));
     }
 
-    enum Label {
-        SPAM, NEGATIVE_TEXT, TOO_LONG, OK
-    }
-
-    abstract class KeywordAnalyzer implements TextAnalyzer {
-
-        protected abstract String[] getKeywords();
-        protected abstract Label getLabel();
-
-        @Override
-        public Label processText(String text){
-            return null;
-        }
-    }
-
-    class SpamAnalyzer extends KeywordAnalyzer {
-
-        private String[] keywords;
-
-        SpamAnalyzer(String[] keywords){
-            this.keywords = keywords;
-        }
-
-        @Override
-        protected String[] getKeywords(){
-            return keywords;
-        }
-
-        @Override
-        protected Label getLabel(){
-            return Label.SPAM;
-        }
-
-        @Override
-        public Label processText(String text) {
-            boolean spam = false;
-            for (int i = 0; i < keywords.length; i++) {
-                if (text.contains(keywords[i])) {
-                    spam = true;
-                }
-            }
-            if (spam) {
-                return getLabel();
-            } else {
-                return Label.OK;
-            }
-        }
-    }
-
-    class NegativeTextAnalyzer extends KeywordAnalyzer {
-
-        private String[] negative = {
-                ":(",
-                "=(",
-                ":|",
-        };
-
-        @Override
-        protected String[] getKeywords(){
-            return negative;
-        }
-
-        @Override
-        protected Label getLabel(){
-            return Label.NEGATIVE_TEXT;
-        }
-
-        @Override
-        public Label processText(String text) {
-            boolean neg = false;
-            for (int i = 0; i < negative.length; i++) {
-                if (text.contains(negative[i])) {
-                    neg = true;
-                }
-            }
-            if (neg) {
-                return getLabel();
-            } else {
-                return Label.OK;
-            }
-        }
-
-    }
-
-    class TooLongTextAnalyzer implements TextAnalyzer {
-
-        private int maxLength;
-
-        public TooLongTextAnalyzer (int maxLength) {
-            this.maxLength = maxLength;
-        }
-
-        @Override
-        public Label processText (String text) {
-            if (text.length() > maxLength) {
-                return Label.TOO_LONG;
-            } else {
-                return Label.OK;
-            }
-        }
-    }
-
-
-    public static Label checkLabels(TextAnalyzer[] analyzers, String text) {
-
-        if (analyzers[0].processText(text).equals(Label.SPAM))
-            return Label.SPAM;
-
-        if (analyzers[0].processText(text).equals(Label.OK) &
-                analyzers[1].processText(text).equals(Label.NEGATIVE_TEXT))
-            return Label.NEGATIVE_TEXT;
-
-        if (analyzers[0].processText(text).equals(Label.OK) &
-                analyzers[1].processText(text).equals(Label.OK) &
-                analyzers[2].processText(text).equals(Label.TOO_LONG))
-            return Label.TOO_LONG;
-        else {
-            return Label.OK;
-        }
+    public Boolean[] boolIn(Boolean[] b){
+        for (int i = 0; i < b.length; i++){
+            b[i] = true;
+        }return b;
     }
 }
 
