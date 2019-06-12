f69b18a5090a251abf609481ea4748e3097a4c88
khilkevich igor <khilkevichigor@gmail.com>
2019-06-12 23:59:40 +0300

    4.1.1

--- src/com/jm/Stepik_04_01_01/Main.java
+++ src/com/jm/Stepik_04_01_01/Main.java
@@ -1,4 +1,28 @@
+/**
+ * Реализуйте метод sqrt(), вычисляющий квадратный корень числа. В отличие от Math.sqrt(), это метод при передаче
+ * отрицательного параметра должен бросать исключение java.lang.IllegalArgumentException с сообщением "Expected
+ * non-negative number, got ?", где вместо вопросика будет подставлено фактически переданное в метод число.
+ */
+
 package com.jm.Stepik_04_01_01;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(sqrt(-1));
+    }
+
+
+
+    public static double sqrt(double x) {
+//        if (x < 0){
+////            try {
+////                throw new IllegalArgumentException();
+////            } catch (IllegalArgumentException e){
+////                System.out.println("Expected non-negative number, got " + x);
+////            }
+////
+////        }
+        return Math.sqrt(x);
+
+    }
 }
