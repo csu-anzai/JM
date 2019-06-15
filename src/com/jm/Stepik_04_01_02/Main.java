1b9577d3d7aeb044585cd1e7d2c2885ee0a30458
khilkevich igor <khilkevichigor@gmail.com>
2019-06-15 21:22:41 +0300

    4.2.1

--- src/com/jm/Stepik_04_01_02/Main.java
+++ src/com/jm/Stepik_04_01_02/Main.java
@@ -21,7 +21,7 @@ public class Main {
     public static String getCallerClassAndMethodName() {
         Throwable t = new Throwable(); //в чем разница: через объект Throwable или черер Thread??????
         StackTraceElement[] stackTraceElements = t.getStackTrace();
-//        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+//        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace(); //не правильно
         if (stackTraceElements.length == 2){ //вызов из main
             return null;
         }
