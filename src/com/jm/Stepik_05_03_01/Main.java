9fc75070a4ce649a5c133026e2cc3139ecd1c143
khilkevich igor <khilkevichigor@gmail.com>
2019-07-11 00:50:52 +0300

    5.3.1

--- src/com/jm/Stepik_05_03_01/Main.java
+++ src/com/jm/Stepik_05_03_01/Main.java
@@ -10,35 +10,29 @@ package com.jm.Stepik_05_03_01;
 import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
 
-        byte[] b = {48, 49, 50, 51};
+        byte[] test = {48, 49, 50, 51};
         Charset charset = StandardCharsets.US_ASCII;
 
         try {
-            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(b);
-            readAsString(byteArrayInputStream, charset);
+            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(test);
+            System.out.println(readAsString(byteArrayInputStream, charset));
         } catch (IOException e1) {
             System.out.println(e1.getMessage());
         }
-
     }
 
     public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
         int num = 0;
-        byte[] buf = new byte[100];
-
+        byte[] buf = new byte[5];
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-
-        while ((num = inputStream.read()) != -1) {
+        while ((num = inputStream.read(buf)) != -1) {
             byteArrayOutputStream.write(buf, 0, num);
+            byteArrayOutputStream.flush();
         }
-        byteArrayOutputStream.flush();
-        System.out.println(Arrays.toString(buf));
-
-        return new String(buf);
+        return new String(buf, charset);
     }
 }
