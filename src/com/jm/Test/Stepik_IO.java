0cada886926514ce5918b6f5885611d672c57423
khilkevich igor <khilkevichigor@gmail.com>
2019-07-14 13:18:20 +0300

    5.4.1

--- src/com/jm/Test/Stepik_IO.java
+++ src/com/jm/Test/Stepik_IO.java
@@ -1,20 +1,17 @@
+/**
+ * Какими байтами представляется символ 'Ы' в кодировке UTF-8? Введите десятичные беззнаковые значения байт через
+ * пробел, например, 10 34 254.
+ */
 package com.jm.Test;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 
 public class Stepik_IO {
     public static void main(String[] args) throws IOException {
         int num = 0;
 
-        byte[] buf = new byte[5];
-        char[] cbuf = new char[5];
-
-        Writer writer = new OutputStreamWriter(new ByteArrayOutputStream(), StandardCharsets.US_ASCII);
-
         while ((num = System.in.read()) != -1) {
-            writer.write(cbuf);
-            writer.flush();
+            System.out.println(num);
         }
     }
 }
