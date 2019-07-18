237a00ef88d0dfb13c46be2d87b77a093337e6c4
khilkevich igor <khilkevichigor@gmail.com>
2019-07-19 00:38:48 +0300

    5.3.2 code review

--- src/com/jm/Test/Stepik_05_03_Read.java
+++ src/com/jm/Test/Stepik_05_03_Read.java
@@ -1,10 +1,16 @@
 package com.jm.Test;
 
+import java.io.ByteArrayInputStream;
+
 public class Stepik_05_03_Read {
     public static void main(String[] args) {
-        int a = -1;
-        byte b = (byte) a;
-        System.out.print(b);
+        byte myByte = -1; // 11111101
+
+        byte[] myArray = {myByte};
+        ByteArrayInputStream in = new ByteArrayInputStream(myArray);
+
+        int myInt = in.read();
+        System.out.println(myInt); // 255
     }
 
 
