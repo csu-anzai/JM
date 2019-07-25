37703cece77f0b6ee9f056006f2895b47153922a
khilkevich igor <khilkevichigor@gmail.com>
2019-07-25 23:02:31 +0300

    multyline comment

--- src/com/jm/Stepik_06_03_01/Main.java
+++ src/com/jm/Stepik_06_03_01/Main.java
@@ -19,7 +19,7 @@ import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
-        
+
     }
 
     public static <T, U> Function<T, U> ternaryOperator(
