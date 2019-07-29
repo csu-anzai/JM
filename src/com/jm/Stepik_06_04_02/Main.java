aad846916e84fafb29d8af598c53e6373a47b11e
khilkevich igor <khilkevichigor@gmail.com>
2019-07-29 21:52:43 +0300

    6.4.2

---
+++ src/com/jm/Stepik_06_04_02/Main.java
@@ -0,0 +1,15 @@
+/*
+Напишите метод, находящий в стриме минимальный и максимальный элементы в соответствии порядком, заданным Comparator'ом.
+Найденные минимальный и максимальный элементы передайте в minMaxConsumer следующим образом:
+minMaxConsumer.accept(min, max);
+Если стрим не содержит элементов, то вызовите
+minMaxConsumer.accept(null, null);
+ */
+
+package com.jm.Stepik_06_04_02;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
