ae78671d1998bcdb22f45668f6b59ac8e724efca
khilkevich igor <khilkevichigor@gmail.com>
2019-07-29 23:55:32 +0300

    6.4.2

--- src/com/jm/Stepik_06_04_02/Main.java
+++ src/com/jm/Stepik_06_04_02/Main.java
@@ -8,9 +8,27 @@ minMaxConsumer.accept(null, null);
 
 package com.jm.Stepik_06_04_02;
 
+import java.math.BigInteger;
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.function.BiConsumer;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
 
     }
+
+    public static <T> void findMinMax(
+            Stream<? super T> stream,
+            Comparator<? super T> order,
+            BiConsumer<? super T, ? super T> minMaxConsumer) {
+
+
+        if (stream.count() == 0) {
+            minMaxConsumer.accept(null, null);
+        } else {
+            minMaxConsumer.accept();
+        }
+    }
 }
- 
