0ad862c1dc45f61c76d2e3b6def05f5265b55ac5
khilkevich igor <khilkevichigor@gmail.com>
2019-07-31 23:31:29 +0300

    6.4.3

--- src/com/jm/Stepik_06_04_03/Main.java
+++ src/com/jm/Stepik_06_04_03/Main.java
@@ -35,5 +35,43 @@ mi
 
 package com.jm.Stepik_06_04_03;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        Charset charset = StandardCharsets.UTF_8;
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in, charset);
+        BufferedReader reader = new BufferedReader(inputStreamReader);
+
+//        List<String> list = new ArrayList<>();
+//        while (true) {
+//            list.add(reader.readLine());
+//            System.out.println(list.toString());
+//        }
+//        Stream<String> stringStream = reader.lines();
+//        stringStream.filter(s -> s.length() > 4)
+////                .peek(s -> s.)
+//                .map(String::toLowerCase)
+//                .forEach(System.out::println);
+
+//        Stream.of(new Scanner(System.in, charset).nextLine().split("\\s+"))
+//                .filter(s -> s.matches("[^A-Za-zА-Яа-я0-9]")
+
+        Stream<String> stringStream = reader.lines();
+        stringStream.filter(s -> s.matches("[^A-Za-zА-Яа-я0-9]"))
+                .map(String::toLowerCase)
+                .forEach(System.out::println);
+
+
+    }
 }
+
